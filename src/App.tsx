0568c1b32b1943a3f63ab818e46e20a27a276355
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-11-24 14:22:38 +0100

    ts-3-lesson-done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TodoItem from "./components/TodoItem";
+import AddTodoForm from "./components/AddTodoForm";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, deleteTodo } from "./todoSlice";
+import { RootState } from "./store";
+
+export interface Todo {
+  id: number;
+  text: string;
+}
 
 function App() {
+  const todos = useSelector((state: RootState) => state.todos);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <AddTodoForm onAdd={(text) => dispatch(addTodo(text))} />
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onDelete={(id) => dispatch(deleteTodo(id))}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
